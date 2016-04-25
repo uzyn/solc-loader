90517be2f077c5d58613393ece5435742c1529a0
U-Zyn Chua <chua@uzyn.com>
2016-04-25 15:53:03 +0800

    Supports optimize query

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 'use strict';
+var parseQuery = require('loader-utils').parseQuery;
 var solc = require('solc');
 
 module.exports = function (source) {
   this.cacheable && this.cacheable();
+  var query = parseQuery(this.query);
+  var optimize = 1;
+  if (query.hasOwnProperty('optimize')) {
+    optimize = (query.optimize) ? '1' : '0';
+  }
 
-  var compiled = solc.compile(source, 1);
+  var compiled = solc.compile(source, optimize);
   var results = {};
 
   for (var name in compiled.contracts) {
