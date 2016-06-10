97ef68cf30a69f43d16953c28a1b44fa14d5ed38
Antanas Bastys <antanas.bastys@tieto.com>
2016-06-10 15:51:10 +0300

    Added support for Solidity 'import' statements, upgraded solc dep version

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict';
 var loaderUtils = require('loader-utils');
 var solc = require('solc');
+var path = require("path");
+var fs = require("fs");
 
 module.exports = function (source) {
   this.cacheable && this.cacheable();
@@ -19,17 +21,32 @@ module.exports = function (source) {
     }
   }
 
-  var compiled = solc.compile(source, optimize);
-  var results = {};
+  var loaderContex = this;
+  var sourceParsedPath = path.parse(loaderContex.resource);
+  var sources = {};
+  sources[sourceParsedPath.base] = source;
+  var input = { sources: sources };
+  var compiled = solc.compile(input, optimize, function (importPath) {
+    var fullPath = path.resolve(sourceParsedPath.dir, importPath);
+    try {
+      var contractSource = fs.readFileSync(fullPath).toString("utf-8");
+      loaderContex.addDependency(fullPath);
+      return { contents: contractSource };
+    } catch (e) {
+      return { error: e };
+    }
+  });
 
   if ('errors' in compiled) {
     throw new Error(compiled.errors.join('\n'));
   }
 
+  var results = {};
   for (var name in compiled.contracts) {
     var contract = compiled.contracts[name];
     if (compiled.contracts.hasOwnProperty(name)) {
       results[name] = {
+        name: name,
         abi: JSON.parse(contract.interface),
         bytecode: contract.bytecode,
       };
