3ae73abd9ed1bcd413cc162cf788470b2af3821d
U-Zyn Chua <chua@uzyn.com>
2016-10-25 23:34:40 +0800

    Refactor. Removed unnecessary loader injection

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 'use strict';
 var loaderUtils = require('loader-utils');
 var solc = require('solc');
-var path = require("path");
-var fs = require("fs");
+var path = require('path');
+var fs = require('fs');
 
 module.exports = function (source) {
   this.cacheable && this.cacheable();
@@ -21,20 +21,12 @@ module.exports = function (source) {
     }
   }
 
-  var loaderContex = this;
-  var sourceParsedPath = path.parse(loaderContex.resource);
-  var sources = {};
-  sources[sourceParsedPath.base] = source;
-  var input = { sources: sources };
-  var compiled = solc.compile(input, optimize, function (importPath) {
-    var fullPath = path.resolve(sourceParsedPath.dir, importPath);
-    try {
-      var contractSource = fs.readFileSync(fullPath).toString("utf-8");
-      loaderContex.addDependency(fullPath);
-      return { contents: contractSource };
-    } catch (e) {
-      return { error: e };
-    }
+  var solPath = path.parse(this.resource);
+  var compiled = solc.compile({ sources: {
+    [solPath.base]: source
+  }}, optimize, function (importPath) {
+    var importSource = fs.readFileSync(path.resolve(solPath.dir, importPath)).toString('utf8');
+    return { contents: importSource };
   });
 
   if ('errors' in compiled) {
